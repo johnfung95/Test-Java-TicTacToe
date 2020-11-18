376b95b9efdb53835b918ea7b9b8d68725bbbba1
johnfung95 <59725906+johnfung95@users.noreply.github.com>
2020-11-18 20:33:18 +0800

    Bug fix
    Changes on:
    	- Checking winner
    	- Asking for inputs
    
    Note:
    	- need to settle drawing situation

--- Game.java
+++ Game.java
@@ -2,27 +2,28 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Game{
-    static String turn;
+    static String turn = "O";
     static Scanner in;
     static int numInput;
 
     public static void main(String[] args) {
         Board myBoard = new Board();
         in = new Scanner(System.in);
-        turn = "O";
 
         myBoard.InitEmptyBoard();
         myBoard.printBoard();
-        while (checkWinner(myBoard.board, turn)== false) {
-            AskForInputs(myBoard.board, turn);
+        while (checkWinner(myBoard.board)== false) {
+            System.out.println("It is " + turn + " 's turn. Please input a number to take the slot.");
+            AskForInputs(myBoard.board, turn, false);
             myBoard.printBoard();
         }
+        System.out.println(turn + " is the winner!");
     }
 
-    static boolean checkWinner(String[] board, String turn) {
-        String line = null;
+    static boolean checkWinner(String[] board) {
         boolean winner = false;
-        for (int i = 0;i < 8; i++){
+        for (int i = 0;i < 9; i++){
+            String line = null;
             switch (i) {
                 case 0:
                     line = board[0] + board[1] + board[2];
@@ -48,19 +49,22 @@ class Game{
                 case 7:
                     line = board[2] + board[4] + board[6];
                     break;
+                case 8:
+                    line = board[0] + board[4] + board[8];
+                    break;
             }
-        }
-        if (line.equals("XXX")) {
-            turn = "X";
-            winner = true;
-        } else if (line.equals("OOO")) {
-            turn = "O";
-            winner = true;
+            if (line.equals("XXX")) {
+                turn = "X";
+                winner = true;
+            } else if (line.equals("OOO")) {
+                turn = "O";
+                winner = true;
+            }
         }
         return winner;
     }
 
-    static String AskForInputs(String[] board, String turn) {
+    static void AskForInputs(String[] board, String chess, boolean winner) {
         try{
             numInput = in.nextInt();
             if (!(numInput > 0 && numInput <= 9)) {
@@ -69,17 +73,17 @@ class Game{
         } catch (InputMismatchException e) {
             System.out.println("Invalid input, please re-enter integer values (between 1 - 9).");
         } 
-        if (board[numInput - 1].equals(String.valueOf(numInput))) {
-            board[numInput - 1] = turn;
-            if (turn.equals("X")) {
-                turn = "O";
+        if (winner == false) {
+            if (board[numInput - 1].equals(String.valueOf(numInput))) {
+                board[numInput - 1] = chess;
+                if (chess.equals("X")) {
+                    turn = "O";
+                } else {
+                    turn = "X";
+                }
             } else {
-                turn = "X";
+                System.out.println("Invalid input, slot already taken, please enter another number.");
             }
-        } else {
-            System.out.println("Invalid input, slot already taken, please enter another number.");
         }
-
-        return turn;
     }
 }
